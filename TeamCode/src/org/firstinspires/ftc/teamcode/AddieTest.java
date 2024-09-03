d4e243dd2c7f6e35a3c7e0cd06c3024dfde7e731
asimpson28 <asimpson28@stjosephacademy.org>
2024-09-03 15:54:06 -0500

    Added buttons and a test stick

--- TeamCode/src/org/firstinspires/ftc/teamcode/AddieTest.java
+++ TeamCode/src/org/firstinspires/ftc/teamcode/AddieTest.java
@@ -1,4 +1,5 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -8,9 +9,31 @@ public class AddieTest extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        double leftStickY = 0.0;
+        int buttonPressCount = 0;
+        boolean buttonPressed= false;
+
         waitForStart();
 
         while (opModeIsActive()) {
+
+            leftStickY = -gamepad1.left_stick_y;
+
+          /*  if (gamepad1.a) {
+                buttonPressCount++;
+            } */
+
+            if (gamepad1.a && !buttonPressed){
+                buttonPressed = true;
+            buttonPressCount++;
+        } else if (!gamepad1.a) {
+            buttonPressed = false;
+
+        }
+            telemetry.addData("Left Stick Y", leftStickY);
+            telemetry.addData("press count", buttonPressCount);
+            telemetry.addData("press count Boolean", buttonPressed);
             telemetry.addData("gamepad a pressed", gamepad1.a);
             telemetry.update();
         }
